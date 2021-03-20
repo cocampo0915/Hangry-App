2deb62a64456782522fa67e96a1336ca487f3428
Camille Ocampo <cocampo0915@gmail.com>
2021-03-20 13:00:44 -0700

    wip: updated formatting for random render object

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// https://codepen.io/FlorinPop17/pen/WNeggor <-- refer to this
-
 // CONSTANTS VARIABLES
 const BASE_URL = 'https://www.themealdb.com/api/json/v1/1/random.php' // pulls a random meal
 
@@ -10,6 +8,7 @@ const BASE_URL = 'https://www.themealdb.com/api/json/v1/1/random.php' // pulls a
 const $recipeName = $('#recipe-name');
 const $recipeImg = $('#recipe-img');
 const $recipeInstructions = $('#recipe-instructions');
+const $ingredients = $('#recipe-ingredients')
 const $recipeVideo = $('#recipe-video');
 
 // EVENT LISTENERS
@@ -24,30 +23,58 @@ function init() {
 }
 
 function getData() {
-    event.preventDefault();
-    $.ajax(BASE_URL)
-        .then(
-          // SUCCESS 
-          (data) => {
-            console.log('data', data);  
-            render(data);
-        }, 
-        // FAILURE
-        (error) => {
-            console.log(error);
-        });
+  event.preventDefault();
+  $.ajax(BASE_URL)
+    .then(
+      // SUCCESS 
+      (data) => {
+        console.log('data', data);
+        render(data);
+      },
+      // FAILURE
+      (error) => {
+        console.log(error);
+      });
 }
 
 function render(data) {
-  const recipe = data["meals"][0];
+  const recipe = data["meals"][0]; // assign object call to a shorter variable for simpler call later
+  // for recipe name title
   $recipeName.html(`
   <h2>
-  ${data["meals"][0].strMeal}
+  ${recipe.strMeal}
   </h2>`);
+  // for recipe image
   $recipeImg.html(`
-  <img src='${data["meals"][0].strMealThumb}' alt="meal image" height="300">`);
-  $recipeInstructions.html(`<p>
-  ${data["meals"][0].strInstructions}
+  <img src='${recipe.strMealThumb}' alt="meal image" height="400">`);
+  // for recipe ingredients
+  const ingredients = []; // Get all ingredients from object and assign to array
+  for (let i = 1; i <= 20; i++) {
+    if (recipe[`strIngredient${i}`]) {
+      ingredients.push(`${recipe[`strIngredient${i}`]} - ${recipe[`strMeasure${i}`]}`);
+    } else {
+      // Stop if no more ingredients
+      break;
+    }
+  }
+  $ingredients.html(`
+  <h4>Ingredients:</h4>
+  <ul>
+  ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+  </ul>
+  `);
+
+  // for recipe instructions
+  let recipeInstructions = recipe.strInstructions;
+
+  recipeInstructions = recipeInstructions.replace(/(?:\r\n|\r|\n)/g, '<br>');
+  $recipeInstructions.html(`
+  <h4>Instructions:</h4>
+  <p>
+  ${recipeInstructions}
   </p>`);
-  $recipeVideo.html(`<iframe width="560" height="315" src=${recipe.strYoutube} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
+
+  // for embed youtube video
+  $recipeVideo.html(`<iframe width="560" height="315" src="https://www.youtube.com/embed/${recipe.strYoutube.slice(-11)}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
 }
+
