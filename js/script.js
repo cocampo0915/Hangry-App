502539e5670fad6a7243fc6c11c026938e08589f
Camille Ocampo <cocampo0915@gmail.com>
2021-03-20 18:28:35 -0700

    wip: added dropdown menu functionality, updated README, updated styling

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 // CONSTANTS VARIABLES
-const BASE_URL = 'https://www.themealdb.com/api/json/v1/1/random.php' // pulls a random meal
+const RANDOM_URL = 'https://www.themealdb.com/api/json/v1/1/random.php' // pulls a random meal
+const CHOICE_URL = 'https://www.themealdb.com/api/json/v1/1/filter.php' // filters by query
+const ID_URL = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=' // pulls by meal ID
+const FILTER_CUISINE = '?a=';
+const FILTER_CATEGORY = '?c=';
 
 // STATE VARIABLES
-// let stories;
+let userCuisine;
+let userCategory;
+let result;
+let finalUrl;
 
 // CACHED ELEMENT REFERENCES
 const $recipeName = $('#recipe-name');
@@ -10,25 +17,89 @@ const $recipeImg = $('#recipe-img');
 const $recipeInstructions = $('#recipe-instructions');
 const $ingredients = $('#recipe-ingredients')
 const $recipeVideo = $('#recipe-video');
+const $cuisine = $('select[id="cuisine"]');
+const $test = $('#test');
 
 // EVENT LISTENERS
-$('#random-meal').on('click', getData);
+$('#random-meal').on('click', randomMeal);
+$('#submit-cuisine').on('click', pullCuisine);
+$('#submit-category').on('click', pullCategory);
+
 
 // FUNCTIONS
 
-// init();
+function randomMeal() {
+  finalUrl = RANDOM_URL;
+  getData();
+}
 
-function init() {
-  console.log("Hello");
+// for capturing user selection from cuisine or category
+$(document).ready(function () {
+  $('#cuisine').change(function () {
+    userCuisine = FILTER_CUISINE + $(this).val();
+    localStorage.setItem('cuisine', $(this).val());
+  });
+  $('#category').change(function () {
+    userCategory = FILTER_CATEGORY + $(this).val();
+    localStorage.setItem('category', $(this).val());
+  });
+});
+
+function pullCuisine() {
+  event.preventDefault();
+  $.ajax(CHOICE_URL + userCuisine)
+    .then(
+      // SUCCESS
+      (data) => {
+        result = fetch(data);
+        // call function to handle fetch new api and render
+        finalUrl = ID_URL + result;
+        getData();
+      },
+      // FAILURE
+      (error) => {
+        console.log(error);
+      });
+}
+
+function pullCategory() {
+  event.preventDefault();
+  $.ajax(CHOICE_URL + userCategory)
+    .then(
+      // SUCCESS
+      (data) => {
+        result = fetch(data);
+        // call function to handle fetch new api and render
+        finalUrl = ID_URL + result;
+        getData();
+      },
+      // FAILURE
+      (error) => {
+        console.log(error);
+      });
+}
+
+function fetch(data) {
+  const recipe = data['meals'];
+// place meal IDs into array
+  const meals = [];
+  for (let i = 0; i < recipe.length; i++) {
+    meals.push(recipe[i].idMeal);
+  }
+// select random value from ID array
+  let randomIdx = getRandomNumber(meals.length);
+  return meals[randomIdx];
+}
+
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 function getData() {
-  event.preventDefault();
-  $.ajax(BASE_URL)
+  $.ajax(finalUrl)
     .then(
       // SUCCESS 
       (data) => {
-        console.log('data', data);
         render(data);
       },
       // FAILURE
@@ -66,15 +137,12 @@ function render(data) {
 
   // for recipe instructions
   let recipeInstructions = recipe.strInstructions;
-
-  recipeInstructions = recipeInstructions.replace(/(?:\r\n|\r|\n)/g, '<br>');
+  recipeInstructions = recipeInstructions.replace(/(?:\r\n|\r|\n)/g, '<br>'); // string has regex expressions, replace with <br> html tag so it formats properly
   $recipeInstructions.html(`
   <h4>Instructions:</h4>
-  <p>
-  ${recipeInstructions}
-  </p>`);
+  <p>${recipeInstructions}</p>
+  `);
 
   // for embed youtube video
   $recipeVideo.html(`<iframe width="560" height="315" src="https://www.youtube.com/embed/${recipe.strYoutube.slice(-11)}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
 }
-
